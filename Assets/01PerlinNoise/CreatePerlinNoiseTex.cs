cd8f692993a34194be618fa6d1ef25c82f46edeb
luckyCatMiao <360382992@qq.com>
2021-04-21 19:18:02 +0800

    add perlin noise Signed-off-by: luckyCatMiao <360382992@qq.com>

---
+++ Assets/01PerlinNoise/CreatePerlinNoiseTex.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace _01PerlinNoise
+{
+    public class CreatePerlinNoiseTex : MonoBehaviour
+    {
+        void Start()
+        {
+            Texture2D texture = new Texture2D(128, 128);
+
+            this.GetComponent<Renderer>().material.mainTexture = texture;
+
+            for (int y = 0; y < texture.height; y++)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+                    float grayscale = PerlinNoise.perlin(x / 128f, y / 128f);
+                    texture.SetPixel(x, y, new Color(grayscale,grayscale,grayscale));
+                }
+            }
+
+            texture.Apply();
+            saveTexture2D(texture, "tex");
+        }
+
+
+        void saveTexture2D(Texture2D texture, string fileName)
+        {
+            var bytes = texture.EncodeToPNG();
+            var file = File.Create(Application.dataPath + "/" + fileName + ".png");
+            var binary = new BinaryWriter(file);
+            binary.Write(bytes);
+            file.Close();
+            UnityEditor.AssetDatabase.Refresh();
+        }
+    }
+}
--- Assets/01PerlinNoise/PerlinNoise.cs
+++ Assets/01PerlinNoise/PerlinNoise.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PerlinNoise : MonoBehaviour
+
+public static class PerlinNoise
 {
-    // Start is called before the first frame update
-    void Start()
+    static float interpolate(float a0, float a1, float w)
     {
-        
+        /* // You may want clamping by inserting:
+         * if (0.0 > w) return a0;
+         * if (1.0 < w) return a1;
+         */
+        return (a1 - a0) * w + a0;
+        /* // Use this cubic interpolation [[Smoothstep]] instead, for a smooth appearance:
+         * return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+         *
+         * // Use [[Smootherstep]] for an even smoother result with a second derivative equal to zero on boundaries:
+         * return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
+         */
     }
 
-    // Update is called once per frame
-    void Update()
+    static Vector2 randomGradient(int ix, int iy)
     {
-        
+        double random = 2920f * Mathf.Sin(ix * 21942 + iy * 171324 + 8912) * Math.Cos(ix * 23157 * iy * 217832 + 9758);
+        return new Vector2(Mathf.Sin((float) random), Mathf.Cos((float) random));
+    }
+
+
+    static float dotGridGradient(int ix, int iy, float x, float y)
+    {
+        // Get gradient from integer coordinates
+        Vector2 gradient = randomGradient(ix, iy);
+
+        // Compute the distance vector
+        float dx = x - (float) ix;
+        float dy = y - (float) iy;
+
+        // Compute the dot-product
+        return (dx * gradient.x + dy * gradient.y);
+    }
+
+
+    public static float perlin(float x, float y)
+    {
+        int x0 = 0;
+        int x1 = 1;
+        int y0 = 0;
+        int y1 = 1;
+
+
+        float sx = x;
+        float sy = y;
+
+        // Interpolate between grid point gradients
+        float n0, n1, ix0, ix1, value;
+
+        n0 = dotGridGradient(x0, y0, x, y);
+        n1 = dotGridGradient(x1, y0, x, y);
+        ix0 = interpolate(n0, n1, sx);
+
+        n0 = dotGridGradient(x0, y1, x, y);
+        n1 = dotGridGradient(x1, y1, x, y);
+        ix1 = interpolate(n0, n1, sx);
+
+        value = interpolate(ix0, ix1, sy);
+        return value;
     }
 }
