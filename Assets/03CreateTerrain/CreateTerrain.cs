28a9fbebf97506fa4423659f6cb51d89d12c616d
luckyCatMiao <360382992@qq.com>
2021-04-23 13:03:41 +0800

    add create terrain Signed-off-by: luckyCatMiao <360382992@qq.com>

---
+++ Assets/03CreateTerrain/CreateTerrain.cs
@@ -0,0 +1,21 @@
+﻿using Algorithm;
+using UnityEngine;
+
+public class CreateTerrain : MonoBehaviour
+{
+    
+    void Start()
+    {
+        for (int z = 0; z < 128; z++)
+        {
+            for (int x = 0; x < 128; x++)
+            {
+                float grayscale = PerlinNoise.noise(x / (float) 16, z / (float) 16);
+                var cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+                int cubeY = (int) (grayscale * 20);
+                cube.transform.position = new Vector3(x, cubeY, z);
+                cube.GetComponent<Renderer>().material.color=Color.green;
+            }
+        }
+    }
+}
